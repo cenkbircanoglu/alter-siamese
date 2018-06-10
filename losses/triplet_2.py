4dd476af823349dd9190a7aad78b719f6043de2a
Cenk.Home <cenk.bircanoglu@gmail.com>
2018-06-10 23:58:51 +0300

    triplet new added

--- losses/triplet_2.py
+++ losses/triplet_2.py
@@ -2,6 +2,45 @@ import torch
 import torch.nn.functional as F
 import itertools
 
+# Inherit from Function
+from torch.autograd import Function
+
+
+class LinearFunction(Function):
+
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    # bias is an optional argument
+    def forward(ctx, input, weight, bias=None):
+        ctx.save_for_backward(input, weight, bias)
+        output = input.mm(weight.t())
+        if bias is not None:
+            output += bias.unsqueeze(0).expand_as(output)
+        return output
+
+    # This function has only a single output, so it gets only one gradient
+    @staticmethod
+    def backward(ctx, grad_output):
+        # This is a pattern that is very convenient - at the top of backward
+        # unpack saved_tensors and initialize all gradients w.r.t. inputs to
+        # None. Thanks to the fact that additional trailing Nones are
+        # ignored, the return statement is simple even when the function has
+        # optional inputs.
+        input, weight, bias = ctx.saved_tensors
+        grad_input = grad_weight = grad_bias = None
+
+        # These needs_input_grad checks are optional and there only to
+        # improve efficiency. If you want to make your code simpler, you can
+        # skip them. Returning gradients for inputs that don't require it is
+        # not an error.
+        if ctx.needs_input_grad[0]:
+            grad_input = grad_output.mm(weight)
+        if ctx.needs_input_grad[1]:
+            grad_weight = grad_output.t().mm(input)
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum(0).squeeze(0)
+
+        return grad_input, grad_weight, grad_bias
 
 class TripletMarginLoss2(torch.nn.Module):
     def __init__(self, margin=1.0, p=2, eps=1e-6, swap=True):
@@ -23,7 +62,7 @@ class TripletMarginLoss2(torch.nn.Module):
 
 
 def module_hook(module, grad_input, grad_out):
-    print(module.indices)
+    #print(module.indices)
     print('module hook')
     print('grad input')
     print(grad_input)
@@ -32,8 +71,9 @@ def module_hook(module, grad_input, grad_out):
     tensor = torch.autograd.Variable(torch.zeros(module.input_size, 1).float())
     #return tensor,
     for i,(indice1, indice2,indice3) in enumerate(module.indices):
-        tensor[indice1].add(-1,grad_input[0][i])
-        tensor[indice2].add(-1,grad_input[0][i])
-        tensor[indice3].add(-1,grad_input[0][i])
-        print(tensor)
+        tensor.data[indice1] = tensor.data[indice1] + grad_input[0].data[i]
+        tensor.data[indice2] = tensor.data[indice2] + grad_input[0].data[i]
+        tensor.data[indice3] = tensor.data[indice3] + grad_input[0].data[i]
+
     print('grad_out', tensor)
+    return tensor,
